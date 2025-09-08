a620381cf8fd453586c40cd614e06307c272688f
nafizfuadsharkar <nafizfuadsharkar@gmail.com>
2025-09-09 02:14:31 +0600

    categories added

--- script/index.js
+++ script/index.js
@@ -1,4 +1,42 @@
 const cart = [];
+// category load
+const loadCategory = () => {
+  fetch("https://openapi.programming-hero.com/api/categories")
+    .then((res) => res.json())
+    .then((json) => displayCategory(json.categories));
+};
+
+const displayCategory = (plants) => {
+  const categoryContainer = document.getElementById("category-container");
+  console.log(plants);
+  for (let plant of plants) {
+    const btnCat = document.createElement("div");
+    btnCat.innerHTML = `
+    <button id="category-btn-${plant.id}" onclick="loadCategoryPlant(${plant.id})" class="text-lg cat-btn">${plant.category_name}</button>
+    `;
+    categoryContainer.append(btnCat);
+  }
+};
+
+const removeActive = () => {
+  const catBtn = document.querySelectorAll(".cat-btn");
+  catBtn.forEach((btn) => btn.classList.remove("active"));
+};
+
+const loadCategoryPlant = (id) => {
+  // load trees
+  const loadTrees = fetch(
+    `https://openapi.programming-hero.com/api/category/${id}`
+  )
+    .then((res) => res.json())
+    .then((json) => {
+      removeActive();
+      const clickBtn = document.getElementById(`category-btn-${id}`);
+      clickBtn.classList.add("active");
+      displayTrees(json.plants);
+    });
+};
+loadCategory();
 
 // load trees
 const loadTrees = fetch("https://openapi.programming-hero.com/api/plants")
@@ -21,8 +59,8 @@ const displayTrees = (trees) => {
           />
         </figure>
         <div class="card-body">
-          <h2 class="card-title">${tree.name}</h2>
-          <p class="h-32">${tree.description}</p>
+          <h2 onclick="loadWordDetail(${tree.id})" class="card-title">${tree.name}</h2>
+          <p class="min-h-32">${tree.description}</p>
           <div class="card-actions justify-between">
             <div class="badge rounded-full bg-[#DCFCE7] text-[#15803D]">${tree.category}</div>
             <div class="font-bold"><p>৳<span>${tree.price}</span></p></div>
@@ -49,6 +87,35 @@ const displayTrees = (trees) => {
       addToCart();
     });
   }
+};
+
+// modal showing
+const loadWordDetail = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/plant/${id}`;
+  const res = await fetch(url);
+  const details = await res.json();
+  displayWordDetails(details.plants);
+};
+
+const displayWordDetails = (tree) => {
+  const detailsBox = document.getElementById("details-container");
+  detailsBox.innerHTML = `
+    <div class="bg-base-100 w-full">
+      <h2 class="card-title">${tree.name}</h2>
+      <img
+        class="rounded-2xl h-64 w-full object-cover object-center"
+        src="${tree.image}"
+        alt="${tree.name}"
+      />
+      <div>
+        <b>Category: </b>${tree.category}</div>
+      <div class="font-bold">
+        <p><b>Price:</b> ৳<span>${tree.price}</span></p>
+      </div>
+      <div><p class=""><b>Description:</b>${tree.description}</p></div>
+    </div>
+  `;
+  document.getElementById("plant_modal").showModal();
 };
 
 // add to cart
